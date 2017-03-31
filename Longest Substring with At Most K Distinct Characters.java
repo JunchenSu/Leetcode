acc0549f37b7df9d1cd278bf826e3c9bd8528499
JunchenSu <js9133@nyu.edu>
2017-03-31 16:35:48 -0400

    Create Longest Substring with At Most K Distinct Characters.java

---
+++ Longest Substring with At Most K Distinct Characters.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if(s.length() < 1) return 0;
+        HashMap<Character,Integer> index = new HashMap<Character,Integer>();
+        int lo = 0;
+        int hi = 0;
+        int maxLength = 0;
+        while(hi < s.length()) {
+            if(index.size() <= k) {
+                char c = s.charAt(hi);
+                index.put(c, hi);
+                hi++;
+            }
+            if(index.size() > k) {
+                int leftMost = s.length();
+                for(int i : index.values()) {
+                    leftMost = Math.min(leftMost,i);
+                }
+                char c = s.charAt(leftMost);
+                index.remove(c);
+                lo = leftMost+1;
+            }
+            maxLength = Math.max(maxLength, hi-lo);
+        }
+        return maxLength;
+    }
+}
